76daf7237dbc13031cc5f55fe80bc6aec04dd31f
kasunsk <kasunsk@gmail.com>
2014-01-03 23:59:40 +0530

    added log4j.properties and added a loger in control class

--- src/main/java/com/codetutr/controller/SampleController.java
+++ src/main/java/com/codetutr/controller/SampleController.java
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 public class SampleController {
-//skasun	
+	
+	private static final Logger log = LoggerFactory.getLogger(SampleController.class);
+	
 	@RequestMapping("home")
 	public String loadHomePage(Model m) {
+		log.info("Entered to the loadHomePage method in SampleController ");
 		m.addAttribute("name", "CodeTutr");
 		return "home";
 	}
